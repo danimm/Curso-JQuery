156d255ead7fa0aa3e19a6d3ab76a48310038a7b
danimm <danimm1989@gmail.com>
2017-07-23 19:10:18 +0200

    terminando el 1bloque de JQuery

--- tvify/app.js
+++ tvify/app.js
@@ -1,1 +1,23 @@
-alert('hola toto');
+// window.onload = function () { // cuando todo esta cargado img tb
+//   alert('loaded');
+// }
+
+$(function () {
+  // var header = document.getElementById('app-header');
+  var a = $('<a>', {
+    href: 'http://platzi.com',
+    target: '_blank',
+    html:'Ir a Platzi'
+  })
+  $('#app-body').append(a);
+
+  // $('header#app-header') esto es sinonimo de lo siguiente
+  // .append($('<p>', { html: 'Me acaban de crear'}))
+
+  // $('<p>', { html: 'Me acaban de crear'})
+  // .appendTo($('header#app-header'))
+
+  // console.log(a.attr('href'));  // getterd
+  console.log(a.attr('href', 'http://google.com'));
+  a.html('Ir a google') // setter modifica
+})
