2b531e28457c145093fe29c3335714bb42672597
danimm <danimm1989@gmail.com>
2017-07-25 18:49:22 +0200

    añadiendo busqueda de los shows

--- tvify/app.js
+++ tvify/app.js
@@ -24,13 +24,24 @@
 
 
 $(function() {
-  /**
-  Submit search form
-  */
-  // function onsubmit(ev){
-  //   esto seria otra manera
-  //   y luego abajo seria .click(onsubmit) llamando a la funcion
-  // }
+  var $tvShowsContainer = $('#app-body').find('.tv-shows')
+
+  function renderShows (shows) {
+    $tvShowsContainer.find('.loader').remove();
+    shows.forEach(function (show) {
+      var article = template
+        .replace(':name:', show.name)
+        .replace(':img:', show.image.medium)
+        .replace(':summary:', show.summary)
+        .replace(':img alt:', show.name + " Logo")
+
+      var $article = $(article);
+      $article.hide  ();
+      $tvShowsContainer.append($article)
+      $article.show("slide", 1200);
+  })
+}
+
 
   $('#app-body')
     .find('form')
@@ -39,9 +50,26 @@ $(function() {
       var busqueda = $(this)
         .find('input[type="text"]')
         .val();
-        alert('Se ha buscado: ' + busqueda);
-    })
 
+
+        $tvShowsContainer.find('.tv-show').remove();
+        var $loader = $('<div class="loader">');
+        $loader.appendTo($tvShowsContainer);
+        $.ajax({
+          url: 'http://api.tvmaze.com/search/shows',
+          data: { q: busqueda },
+          success: function(res, textStatus, xhr) {
+            $loader.remove();
+            var shows = res.map(function (el) {
+              return el.show;
+            })
+            renderShows(shows);
+          }
+        })
+    })
+    // $tvShowsContainer.find('.tv-show').remove()
+      // var $loader = $('<div class="loader">');
+      // $loader.appendTo($tvShowsContainer);
     var template = '<article class="tv-show">' +
                       '<div class="left img-container">' +
                         '<img src=":img:" alt=":img alt:">' +
@@ -50,23 +78,12 @@ $(function() {
                         '<h1>:name:</h1>' +
                         "<p>:summary:</p>" +
                       '</div>' +
-                    '</article>'
-
-    $.ajax({
-      url: 'http://api.tvmaze.com/shows',
-      success: function(shows, textStatus, xhr){
-        // es un callback ejecuta la funcion por cada elemento de arreglo
-        var $tvShowsContainer = $('#app-body').find('.tv-shows')
-        shows.forEach(function (show) {
-          var article = template
-            .replace(':name:', show.name)
-            .replace(':img:', show.image.medium)
-            .replace(':summary:', show.summary)
-            .replace(':img alt:', show.name + " Logo")
-
-          $tvShowsContainer.append($(article))
-
-        })
-      }
-    })
+                    '</article>';
+  $.ajax({
+  url: 'http://api.tvmaze.com/shows',
+  success: function(shows, textStatus, xhr) {
+    $tvShowsContainer.find('.loader').remove();
+    renderShows(shows);
+  }
+})
 })
